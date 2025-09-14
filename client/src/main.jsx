fda1ddeeb0fba923bc97a3f89efa9945200bc29b
rcd-env <donerakesh001@gmail.com>
2025-09-14 15:54:12 +0530

    add Navbar and Footer components to main application layout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,9 +8,12 @@ import NotFoundPage from "./pages/NotFoundPage";
 import PartnerRegister from "./pages/register/PartnerRegister";
 import PartnerLogin from "./pages/login/PartnerLogin";
 import UserLogin from "./pages/login/UserLogin";
+import Navbar from "./pages/Navbar";
+import Footer from "./pages/Footer";
 
 createRoot(document.getElementById("root")).render(
   <Router>
+    <Navbar />
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/user/register" element={<UserRegister />} />
