97b5f1307a30c4f1954e4371e0b9eda174040a32
rcd-env <donerakesh001@gmail.com>
2025-09-15 01:29:30 +0530

    fix: update food partner routes to use Dashboard and CreateFood components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,20 +10,19 @@ import PartnerLogin from "./pages/login/PartnerLogin";
 import UserLogin from "./pages/login/UserLogin";
 import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
+import Dashboard from "./pages/partner/Dashboard";
+import CreateFood from "./pages/partner/CreateFood";
 
 createRoot(document.getElementById("root")).render(
   <Router>
-    <Navbar />
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/user/register" element={<UserRegister />} />
       <Route path="/user/login" element={<UserLogin />} />
       <Route path="/food-partner/register" element={<PartnerRegister />} />
       <Route path="/food-partner/login" element={<PartnerLogin />} />
-      <Route
-        path="/food-partner/dashboard"
-        element={<div>Food Partner Dashboard</div>}
-      />
+      <Route path="/food-partner/dashboard" element={<Dashboard />} />
+      <Route path="/food-partner/create" element={<CreateFood />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </Router>
